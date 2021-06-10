5ec9957fbff81eb0db175221314a0ac57bf96f28
Dominic Alie <dominic@georadix.com>
2021-06-09 21:55:56 -0400

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { F1TracksComponent } from './scenarios/f1-tracks/f1-tracks.component';
 import { ScenarioComponent } from './scenarios/scenario/scenario.component';
 
 const routes: Routes = [
-  {
-    path: 'scenario/f1-tracks',
-    component: F1TracksComponent,
-  },
   {
     path: 'scenario/:scenarioId',
     component: ScenarioComponent,
